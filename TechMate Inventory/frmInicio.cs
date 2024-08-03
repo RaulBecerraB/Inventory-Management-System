6a59337c1b061e57464dfcb037aea99db579b1f8
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2024-08-03 11:09:50 -0600

    chore (login): remove test credentials from login screen
        - Removed test credentials for security reasons from the login screen.

--- TechMate Inventory/frmInicio.cs
+++ TechMate Inventory/frmInicio.cs
@@ -24,59 +24,8 @@ namespace TechMate_Inventory
 
         private void frmInicio_Load(object sender, EventArgs e)
         {
-            frmBorrowings borrowings = new frmBorrowings(connectionString);
-            LoadBorrowingsView();
+
         }
 
-        public void LoadBorrowingsView()
-        {
-            string query = @"
-SELECT 
-    B.ID_Borrowing,
-    M.ID_Material,
-    S.Matricula,
-    M.shortDescription,
-    U.Name AS UserName,
-    S.name AS StudentName,
-    B.quantity,
-    B.return_date,
-    B.borrow_date,
-    B.isBorrowed
-FROM 
-    Borrowings B
-JOIN 
-    Materials M ON B.ID_Material = M.ID_Material
-JOIN 
-    Users U ON B.ID_User = U.ID_User
-JOIN 
-    Students S ON B.Matricula = S.Matricula";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    connection.Open();
-
-                    DataTable borrowingsDataTable = Program.GetDataTable(query, connection);
-                    vwBorrowingsGridView.DataSource = borrowingsDataTable;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("ERROR cargando la gridView: " + ex.Message);
-                }
-            }
-
-            vwBorrowingsGridView.Columns["ID_Material"].Visible = false;
-            vwBorrowingsGridView.Columns["ID_Borrowing"].Visible = false;
-            vwBorrowingsGridView.Columns["Matricula"].Visible = false;
-            vwBorrowingsGridView.Columns["isBorrowed"].Visible = false;
-
-            DGridViewUtils.RenameDGVColumn(vwBorrowingsGridView, "shortDescription", "Material");
-            DGridViewUtils.RenameDGVColumn(vwBorrowingsGridView, "UserName", "Prestamista");
-            DGridViewUtils.RenameDGVColumn(vwBorrowingsGridView, "StudentName", "Estudiante");
-            DGridViewUtils.RenameDGVColumn(vwBorrowingsGridView, "quantity", "Cantidad prestada");
-            DGridViewUtils.RenameDGVColumn(vwBorrowingsGridView, "return_date", "Fecha máxima de retorno");
-            DGridViewUtils.RenameDGVColumn(vwBorrowingsGridView, "borrow_date", "Fecha del préstamo");
-        }
     }
 }
--- TechMate Inventory/frmLogin.Designer.cs
+++ TechMate Inventory/frmLogin.Designer.cs
@@ -40,47 +40,48 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnHidePwd = new System.Windows.Forms.PictureBox();
             this.forceLoginBtn = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnHidePwd)).BeginInit();
             this.SuspendLayout();
             // 
             // catMatTitle
             // 
-            this.catMatTitle.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.catMatTitle.Location = new System.Drawing.Point(133, 49);
+            this.catMatTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.catMatTitle.Location = new System.Drawing.Point(100, 40);
+            this.catMatTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.catMatTitle.Name = "catMatTitle";
-            this.catMatTitle.Size = new System.Drawing.Size(137, 61);
+            this.catMatTitle.Size = new System.Drawing.Size(103, 50);
             this.catMatTitle.TabIndex = 0;
             this.catMatTitle.Text = "Login";
             // 
             // textBoxUserName
             // 
             this.textBoxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxUserName.Location = new System.Drawing.Point(69, 169);
-            this.textBoxUserName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxUserName.Location = new System.Drawing.Point(52, 137);
+            this.textBoxUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxUserName.Name = "textBoxUserName";
-            this.textBoxUserName.Size = new System.Drawing.Size(274, 34);
+            this.textBoxUserName.Size = new System.Drawing.Size(206, 28);
             this.textBoxUserName.TabIndex = 2;
             this.textBoxUserName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxUserName_KeyPress);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(64, 132);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(48, 107);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(150, 25);
+            this.label6.Size = new System.Drawing.Size(143, 20);
             this.label6.TabIndex = 1;
             this.label6.Text = "Nombre de usuario";
             // 
             // btnLogin
             // 
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(69, 314);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLogin.Location = new System.Drawing.Point(52, 255);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(274, 50);
+            this.btnLogin.Size = new System.Drawing.Size(206, 41);
             this.btnLogin.TabIndex = 5;
             this.btnLogin.Text = "Iniciar sesión";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -89,10 +90,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(64, 214);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 174);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 25);
+            this.label1.Size = new System.Drawing.Size(92, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Contraseña";
             // 
@@ -100,31 +102,33 @@
             // 
             this.textBoxPassword.BackColor = System.Drawing.SystemColors.Window;
             this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPassword.Location = new System.Drawing.Point(69, 252);
-            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxPassword.Location = new System.Drawing.Point(52, 205);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(274, 34);
+            this.textBoxPassword.Size = new System.Drawing.Size(206, 28);
             this.textBoxPassword.TabIndex = 4;
             this.textBoxPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPassword_KeyPress);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(65, 407);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 331);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(143, 25);
+            this.label2.Size = new System.Drawing.Size(138, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "No tienes cuenta?";
             // 
             // btnRegister
             // 
             this.btnRegister.AutoSize = true;
-            this.btnRegister.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegister.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.btnRegister.Location = new System.Drawing.Point(214, 407);
+            this.btnRegister.Location = new System.Drawing.Point(160, 331);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(86, 25);
+            this.btnRegister.Size = new System.Drawing.Size(83, 20);
             this.btnRegister.TabIndex = 7;
             this.btnRegister.Text = "Regístrate";
             this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
@@ -132,9 +136,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(386, -158);
+            this.pictureBox1.Location = new System.Drawing.Point(290, -128);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(397, 667);
+            this.pictureBox1.Size = new System.Drawing.Size(298, 542);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 26;
             this.pictureBox1.TabStop = false;
@@ -143,9 +148,10 @@
             // 
             this.btnHidePwd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.btnHidePwd.Image = ((System.Drawing.Image)(resources.GetObject("btnHidePwd.Image")));
-            this.btnHidePwd.Location = new System.Drawing.Point(310, 252);
+            this.btnHidePwd.Location = new System.Drawing.Point(232, 205);
+            this.btnHidePwd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnHidePwd.Name = "btnHidePwd";
-            this.btnHidePwd.Size = new System.Drawing.Size(33, 34);
+            this.btnHidePwd.Size = new System.Drawing.Size(25, 28);
             this.btnHidePwd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnHidePwd.TabIndex = 27;
             this.btnHidePwd.TabStop = false;
@@ -156,31 +162,21 @@
             this.forceLoginBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.forceLoginBtn.Location = new System.Drawing.Point(499, 22);
+            this.forceLoginBtn.Location = new System.Drawing.Point(374, 18);
+            this.forceLoginBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.forceLoginBtn.Name = "forceLoginBtn";
-            this.forceLoginBtn.Size = new System.Drawing.Size(180, 61);
+            this.forceLoginBtn.Size = new System.Drawing.Size(135, 50);
             this.forceLoginBtn.TabIndex = 8;
             this.forceLoginBtn.Text = "Forzar login (developer)";
             this.forceLoginBtn.UseVisualStyleBackColor = true;
             this.forceLoginBtn.Visible = false;
             this.forceLoginBtn.Click += new System.EventHandler(this.forceLoginBtn_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(588, 392);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(173, 75);
-            this.label4.TabIndex = 29;
-            this.label4.Text = "Credenciales Teresita:\r\nTeresita\r\nAdmin6\r\n";
-            // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(773, 476);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(580, 387);
             this.Controls.Add(this.forceLoginBtn);
             this.Controls.Add(this.btnHidePwd);
             this.Controls.Add(this.btnRegister);
@@ -193,6 +189,7 @@
             this.Controls.Add(this.catMatTitle);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmLogin";
@@ -220,6 +217,5 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox btnHidePwd;
         private System.Windows.Forms.Button forceLoginBtn;
-        private System.Windows.Forms.Label label4;
     }
 }
