eec5d478c36263d828298939c2741fdb95e2810c
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-09-17 17:04:04 -0600

    fix (Login): update DevLogin function and enable force login button
        - Fixed the DevLogin function in the same form as the Login method.
        - Enabled the button to force login.

--- TechMate Inventory/frmLogin.Designer.cs
+++ TechMate Inventory/frmLogin.Designer.cs
@@ -47,20 +47,19 @@
             // catMatTitle
             // 
             this.catMatTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.catMatTitle.Location = new System.Drawing.Point(100, 40);
-            this.catMatTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.catMatTitle.Location = new System.Drawing.Point(133, 49);
             this.catMatTitle.Name = "catMatTitle";
-            this.catMatTitle.Size = new System.Drawing.Size(103, 50);
+            this.catMatTitle.Size = new System.Drawing.Size(137, 62);
             this.catMatTitle.TabIndex = 0;
             this.catMatTitle.Text = "Login";
             // 
             // textBoxUserName
             // 
             this.textBoxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxUserName.Location = new System.Drawing.Point(52, 137);
-            this.textBoxUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxUserName.Location = new System.Drawing.Point(69, 169);
+            this.textBoxUserName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxUserName.Name = "textBoxUserName";
-            this.textBoxUserName.Size = new System.Drawing.Size(206, 28);
+            this.textBoxUserName.Size = new System.Drawing.Size(273, 34);
             this.textBoxUserName.TabIndex = 2;
             this.textBoxUserName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxUserName_KeyPress);
             // 
@@ -68,20 +67,19 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(48, 107);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(64, 132);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(143, 20);
+            this.label6.Size = new System.Drawing.Size(177, 25);
             this.label6.TabIndex = 1;
             this.label6.Text = "Nombre de usuario";
             // 
             // btnLogin
             // 
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(52, 255);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogin.Location = new System.Drawing.Point(69, 314);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(206, 41);
+            this.btnLogin.Size = new System.Drawing.Size(275, 50);
             this.btnLogin.TabIndex = 5;
             this.btnLogin.Text = "Iniciar sesión";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -91,10 +89,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(48, 174);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(64, 214);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.Size = new System.Drawing.Size(114, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Contraseña";
             // 
@@ -102,10 +99,10 @@
             // 
             this.textBoxPassword.BackColor = System.Drawing.SystemColors.Window;
             this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPassword.Location = new System.Drawing.Point(52, 205);
-            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxPassword.Location = new System.Drawing.Point(69, 252);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(206, 28);
+            this.textBoxPassword.Size = new System.Drawing.Size(273, 34);
             this.textBoxPassword.TabIndex = 4;
             this.textBoxPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPassword_KeyPress);
             // 
@@ -113,10 +110,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(49, 331);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(65, 407);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 20);
+            this.label2.Size = new System.Drawing.Size(169, 25);
             this.label2.TabIndex = 6;
             this.label2.Text = "No tienes cuenta?";
             // 
@@ -125,10 +121,9 @@
             this.btnRegister.AutoSize = true;
             this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegister.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.btnRegister.Location = new System.Drawing.Point(160, 331);
-            this.btnRegister.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.btnRegister.Location = new System.Drawing.Point(213, 407);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(83, 20);
+            this.btnRegister.Size = new System.Drawing.Size(99, 25);
             this.btnRegister.TabIndex = 7;
             this.btnRegister.Text = "Regístrate";
             this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
@@ -136,10 +131,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(290, -128);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(387, -158);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(298, 542);
+            this.pictureBox1.Size = new System.Drawing.Size(397, 667);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 26;
             this.pictureBox1.TabStop = false;
@@ -148,10 +143,10 @@
             // 
             this.btnHidePwd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.btnHidePwd.Image = ((System.Drawing.Image)(resources.GetObject("btnHidePwd.Image")));
-            this.btnHidePwd.Location = new System.Drawing.Point(232, 205);
-            this.btnHidePwd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnHidePwd.Location = new System.Drawing.Point(309, 252);
+            this.btnHidePwd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnHidePwd.Name = "btnHidePwd";
-            this.btnHidePwd.Size = new System.Drawing.Size(25, 28);
+            this.btnHidePwd.Size = new System.Drawing.Size(33, 34);
             this.btnHidePwd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnHidePwd.TabIndex = 27;
             this.btnHidePwd.TabStop = false;
@@ -162,21 +157,20 @@
             this.forceLoginBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.forceLoginBtn.Location = new System.Drawing.Point(374, 18);
-            this.forceLoginBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.forceLoginBtn.Location = new System.Drawing.Point(499, 22);
+            this.forceLoginBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.forceLoginBtn.Name = "forceLoginBtn";
-            this.forceLoginBtn.Size = new System.Drawing.Size(135, 50);
+            this.forceLoginBtn.Size = new System.Drawing.Size(180, 62);
             this.forceLoginBtn.TabIndex = 8;
             this.forceLoginBtn.Text = "Forzar login (developer)";
             this.forceLoginBtn.UseVisualStyleBackColor = true;
-            this.forceLoginBtn.Visible = false;
             this.forceLoginBtn.Click += new System.EventHandler(this.forceLoginBtn_Click);
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 387);
+            this.ClientSize = new System.Drawing.Size(773, 476);
             this.Controls.Add(this.forceLoginBtn);
             this.Controls.Add(this.btnHidePwd);
             this.Controls.Add(this.btnRegister);
@@ -189,7 +183,7 @@
             this.Controls.Add(this.catMatTitle);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmLogin";
